8de579c6d90aae98de9297fb8145ab8c6da9f779
TaiseiIto <taisei1212@outlook.jp>
2021-05-30 16:56:35 +0900

    boot.rsまで解読

--- 04_safe_globals/src/_arch/aarch64/cpu/boot.s
+++ 04_safe_globals/src/_arch/aarch64/cpu/boot.s
@@ -7,9 +7,9 @@
 //--------------------------------------------------------------------------------------------------
 
 // Load the address of a symbol into a register, PC-relative.
-//
+// Program Counter相対でsymbolのaddressをregisterに読み込む疑似命令を定義するよ．
 // The symbol must lie within +/- 4 GiB of the Program Counter.
-//
+// symbolはProgram Counterの+/- 4 GiBの範囲内にないといけないよ．
 // # Resources
 //
 // - https://sourceware.org/binutils/docs-2.36/as/AArch64_002dRelocations.html
@@ -18,34 +18,37 @@
 	add	\register, \register, #:lo12:\symbol
 .endm
 
-.equ _core_id_mask, 0b11
+.equ _core_id_mask, 0b11 /* 自分のcore番号を取得するためのmaskだよ */
 
 //--------------------------------------------------------------------------------------------------
 // Public Code
 //--------------------------------------------------------------------------------------------------
-.section .text._start
+.section .text._start /* ここが../../../bsp/__board_name__/link.ldで定義されてるOSのentry pointだよ */
 
 //------------------------------------------------------------------------------
 // fn _start()
 //------------------------------------------------------------------------------
-_start:
+_start: /* entry pointに関数を配置するよ *.
 	// Only proceed on the boot core. Park it otherwise.
+	// 自分のcore番号をx1に取得するよ
 	mrs	x1, MPIDR_EL1
 	and	x1, x1, _core_id_mask
-	ldr	x2, BOOT_CORE_ID      // provided by bsp/__board_name__/cpu.rs
-	cmp	x1, x2
-	b.ne	1f
+	ldr	x2, BOOT_CORE_ID      // provided by ../../../bsp/__board_name__/cpu.rs
+	cmp	x1, x2				  // ../../../bsp/__board_name__/cpu.rsで定義されるboot core番号と自分のcore番号を比較するよ
+	b.ne	1f				  // boot core以外は1に飛んで停止するよ
 
 	// If execution reaches here, it is the boot core. Now, prepare the jump to Rust code.
+	// boot coreだけが以下の命令を実行するよ
 
 	// Set the stack pointer.
-	ADR_REL	x0, __boot_core_stack_end_exclusive
+	ADR_REL	x0, __boot_core_stack_end_exclusive /* ../../../bsp/raspberrypi/link.ldで定義されるstackの底 */
 	mov	sp, x0
 
 	// Jump to Rust code.
-	b	_start_rust
+	b	_start_rust /* ./boot.rsの_srart_rustに飛ぶよ */
 
 	// Infinitely wait for events (aka "park the core").
+	// boot core以外のcoreはwait for eventを無限loopすることで停止するよ
 1:	wfe
 	b	1b
 
