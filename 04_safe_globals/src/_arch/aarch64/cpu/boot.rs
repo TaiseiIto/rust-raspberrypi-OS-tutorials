be7ff182c1cb6bc1f0e3a78b0eaee6c7990a6b24
TaiseiIto <taisei1212@outlook.jp>
2021-06-12 14:18:34 +0900

    動かなくなったので一部コメントアウト

--- 04_safe_globals/src/_arch/aarch64/cpu/boot.rs
+++ 04_safe_globals/src/_arch/aarch64/cpu/boot.rs
@@ -14,7 +14,6 @@
 use crate::runtime_init;
 
 // Assembly counterpart to this file.
-// ./boot.sを取り込むよ．
 global_asm!(include_str!("boot.s"));
 
 //--------------------------------------------------------------------------------------------------
@@ -22,14 +21,13 @@ global_asm!(include_str!("boot.s"));
 //--------------------------------------------------------------------------------------------------
 
 /// The Rust entry of the `kernel` binary.
-/// 
+///
 /// The function is called from the assembly `_start` function.
-/// boot coreだけが./boot.sの_start関数からここに飛んでくるよ．
+///
 /// # Safety
-/// - `bss`領域はまだ初期化されてないから使っちゃだめだよ．
+///
 /// - The `bss` section is not initialized yet. The code must not use or reference it in any way.
 #[no_mangle]
 pub unsafe fn _start_rust() -> ! {
-    // ../../../runtime_init.rsのruntime_initに飛ぶよ．
-    runtime_init::runtime_init() // mainを実行する前に必要な初期化の処理だよ．
+    runtime_init::runtime_init()
 }
